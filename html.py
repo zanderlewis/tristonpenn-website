f60a500598da8f6832bdd7ce804ce2e350cf4911
Zander Lewis <zander@zanderlewis.dev>
2024-09-14 22:13:21 -0400

    push

--- html.py
+++ html.py
@@ -1,13 +1,38 @@
 import os
+from datetime import datetime
 
 dir_path = './images'
 html = ''
 
-for filename in os.listdir(dir_path):
-    if filename.lower().endswith('.jpg') or filename.lower().endswith('.png'):
-        html += f'<div class="image-container"><img src="images/{filename}" alt="Gallery Image"></div>\n'
-    else:
-        raise Exception(f"Incorrect file type for file `{filename}`")
+def get_age(birthdate):
+    # YYYY-MM-DD
+    birthdate = datetime.strptime(birthdate, '%Y-%m-%d')
+    today = datetime.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    # convert 
+    return age
 
-with open('gal.html', 'w') as f:
-    f.write(html)
+def imgs():
+    html = ''
+    for filename in os.listdir(dir_path):
+        if filename.lower().endswith('.jpg') or filename.lower().endswith('.png') or filename.lower().endswith('.jpeg'):
+            html += f'''
+                <div class="image-container">
+                    <a target="_blank" href="{filename}">
+                        <img src="{filename}" alt="Gallery Image" loading="lazy">
+                    </a>
+                </div>
+            '''
+        else:
+            raise Exception(f"Incorrect file type for file `{filename}`")
+    return html
+
+def main():
+    with open('gallery.html', 'w') as f:
+        # find the line with `{{gallery}}` and replace it with the generated html
+        f.write(open('template.html').read().replace('{{gallery}}', html))
+        # write age from birthday and replace `{{age}}` with the calculated age
+        f.write(open('template.html').read().replace('{{age}}', str(get_age('2008-06-06'))))
+                
+if __name__ == '__main__':
+    main()
--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 // main.js
-
-function calculateAge(birthDate) {
-    let today = new Date();
-    let birth = new Date(birthDate);
-    let age = today.getFullYear() - birth.getFullYear();
-    let m = today.getMonth() - birth.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
-        age--;
-    }
-    return age;
-}
-
 window.onload = function () {
     // Try to find the 'darkMode' cookie
     const darkModeCookie = document.cookie.split('; ').find(row => row.startsWith('darkMode='));
